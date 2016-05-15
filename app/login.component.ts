1819272fdc93456b0fab56ca8d58ef8ecdc17f33
asteroidice <ryan.s.rabello@gmail.com>
2016-05-14 22:37:25 -0700

    added firebase

--- app/login.component.ts
+++ app/login.component.ts
@@ -3,8 +3,18 @@ import { Component } from "@angular/core";
 @Component({
   selector: "login",
   template: `<h2> Login</h2>
-  name: <input/>
-  password: <input type="password"/>`
+  <form>
+  <fieldset class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="Enter email">
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Password">
+  </fieldset>
+  </form>
+  <button class="btn btn-primary">Login</button>
+  `
 })
 
 export class Login {
